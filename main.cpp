5a523843297e451a14b0608e75d150dc965abf5e
Dorota Siciak <dorota.siciak.programistka@gmail.com>
2024-07-29 19:58:36 +0200

    fix bug in menu

--- main.cpp
+++ main.cpp
@@ -9,83 +9,74 @@ int main()
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
 
     char wyborMenuGlowne = '0';
+    char wyborMenuUzytkownika = '0';
 
-    while (wyborMenuGlowne != '3')
+    while(true)
     {
-        MetodyPomocnicze::wyswietlMenuGlowne();
-        wyborMenuGlowne = MetodyPomocnicze::wczytajZnak();
-
-        switch (wyborMenuGlowne)
+        if(!ksiazkaAdresowa.czyUzytkownikJestZalogowany())
         {
-        case '1':
-            if(!ksiazkaAdresowa.czyUzytkownikJestZalogowany())
+            MetodyPomocnicze::wyswietlMenuGlowne();
+            wyborMenuGlowne = MetodyPomocnicze::wczytajZnak();
+            switch (wyborMenuGlowne)
             {
-                if(ksiazkaAdresowa.logowanieUzytkownika() == 0)
-                {
-                    break;
-                }
-                char wyborMenuUzytkownika = 0;
-
-                while (wyborMenuUzytkownika != '9')
-                {
-                    MetodyPomocnicze::wyswietlMenuUzytkownika();
-                    wyborMenuUzytkownika = MetodyPomocnicze::wczytajZnak();
-
-                    switch (wyborMenuUzytkownika)
-                    {
-                    case '1':
-                        ksiazkaAdresowa.dodajNowegoAdresata();
-                        break;
-                    case '2':
-                        ksiazkaAdresowa.wyszukajAdresataPoImieniu();
-                        break;
-                    case '3':
-                        ksiazkaAdresowa.wyszukajAdresataPoNazwisku();
-                        break;
-                    case '4':
-                        ksiazkaAdresowa.wypiszWszystkichAdresatowZalogowanegoUzytkownika();
-                        system("pause");
-                        break;
-                    case '5':
-                        ksiazkaAdresowa.usunAdresata();
-                        break;
-                    case '6':
-                        ksiazkaAdresowa.edytujAdresata();
-                        break;
-                    case '7':
-                        ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
-                        break;
-                    case '9':
-                        cout << "Nastapi wylogowanie uzytkownika" << endl;
-                        ksiazkaAdresowa.wylogujUzytkownika();
-                        system("pause");
-                        break;
-                    default:
-                        cout << "Wprowadziles znak nieobslugiwany przez menu programu. Sproboj ponownie." << endl;
-                        system("pause");
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                cout << "Nieudane logowanie." << endl;
+            case '1':
+                ksiazkaAdresowa.logowanieUzytkownika();
+                break;
+            case '2':
+                ksiazkaAdresowa.rejestracjaUzytkownika();
+                break;
+            case '3':
+                cout << "Koncze program" << endl;
+                exit(0);
+                break;
+            default:
+                cout << "Wprowadziles znak nieobslugiwany przez menu programu. Sproboj ponownie." << endl;
                 system("pause");
+                break;
             }
-            break;
-        case '2':
-            ksiazkaAdresowa.rejestracjaUzytkownika();
-            break;
-        case '3':
-            break;
-        default:
-            cout << "Wprowadziles znak nieobslugiwany przez menu programu. Sproboj ponownie." << endl;
-            system("pause");
-            break;
         }
+        else
+        {
+            MetodyPomocnicze::wyswietlMenuUzytkownika();
+            wyborMenuUzytkownika = MetodyPomocnicze::wczytajZnak();
+
+            switch (wyborMenuUzytkownika)
+            {
+            case '1':
+                ksiazkaAdresowa.dodajNowegoAdresata();
+                break;
+            case '2':
+                ksiazkaAdresowa.wyszukajAdresataPoImieniu();
+                break;
+            case '3':
+                ksiazkaAdresowa.wyszukajAdresataPoNazwisku();
+                break;
+            case '4':
+                ksiazkaAdresowa.wypiszWszystkichAdresatowZalogowanegoUzytkownika();
+                system("pause");
+                break;
+            case '5':
+                ksiazkaAdresowa.usunAdresata();
+                break;
+            case '6':
+                ksiazkaAdresowa.edytujAdresata();
+                break;
+            case '7':
+                ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
+                break;
+            case '9':
+                cout << "Nastapi wylogowanie uzytkownika" << endl;
+                ksiazkaAdresowa.wylogujUzytkownika();
+                system("pause");
+                break;
+            default:
+                cout << "Wprowadziles znak nieobslugiwany przez menu programu. Sproboj ponownie." << endl;
+                system("pause");
+                break;
+            }
+        }
+
     }
 
-    cout << "Koncze program" << endl;
-
-    return 0;
+return 0;
 }
